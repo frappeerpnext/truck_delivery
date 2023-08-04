08cac8238012db00bf1590c04322e43e3084f44e
frappeerpnext <frapperpnext@gmail.com>
2023-08-04 09:36:54 +0700

    update system

--- truck_delivery/truck_delivery/doctype/purchase_order/purchase_order.py
+++ truck_delivery/truck_delivery/doctype/purchase_order/purchase_order.py
@@ -8,7 +8,6 @@ from frappe.model.document import Document
 class PurchaseOrder(Document):
 	def validate(self):
 		for d in self.products:
-	
 			d.total_amount = (d.quantity or 1) * (d.price or 0)
 			d.remaining_quantity  = (d.quantity or 0)  -  (d.delivered_quantity or 0)
 
@@ -16,5 +15,12 @@ class PurchaseOrder(Document):
 		self.total_delivered_quantity = Enumerable(self.products).sum(lambda x: x.delivered_quantity or 0)
 		self.total_remaining_quantity = Enumerable(self.products).sum(lambda x: x.remaining_quantity or 0)
 		self.total_amount =  Enumerable(self.products).sum(lambda x: x.total_amount or 0)
+
+	def on_submit(self):
+		if self.request_for_quotation:
+			data = frappe.db.sql("select count(name) as total_order from `tabPurchase Order` where docstatus=1 and request_for_quotation='{}'".format(self.request_for_quotation),as_dict=1)
+			if data:
+				 
+				frappe.db.set_value("Request for Quotation", self.request_for_quotation,"total_purchase_order", data[0]["total_order"])
 
 
