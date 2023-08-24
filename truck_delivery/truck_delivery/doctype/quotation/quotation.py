bfb51fc5e6032a8c32f625f313ac5698f5760ed4
frappeerpnext <frapperpnext@gmail.com>
2023-08-24 15:15:12 +0700

    update quotation

--- truck_delivery/truck_delivery/doctype/quotation/quotation.py
+++ truck_delivery/truck_delivery/doctype/quotation/quotation.py
@@ -28,7 +28,6 @@ class Quotation(Document):
 def get_customer_quotations(start,end,customer_type=None,customer=None):
 	filters = {
 		'start_date': ['>=', start],
-		'end_date': ['<=', end] ,
 		'docstatus': ['=', 1] ,
 
 	}
